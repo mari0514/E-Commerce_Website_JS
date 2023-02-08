8a7222cfad17597a8e0039da3c0783fb04bb10f4
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-02-07 22:53:36 -0800

    shopping cart in progress

--- app.js
+++ app.js
@@ -1,61 +1,88 @@
 window.addEventListener("load", () => {
-    // const API_URL = "http://jsonblob.com/api/1072662121845899264"; 
-    const API_URL = "http://jsonblob.com/api/1072665233168023552"; 
+    // const API_URL = "http://jsonblob.com/api/1072684897273135104";
+    const API_URL = "http://jsonblob.com/api/1072766826269917184";
 
-    function displayItems() {
-        fetch(API_URL)
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                // let itemArray = data;
-                // console.log(itemArray);
-                console.log(data.length);
-                data.forEach(item => {
-                    console.log(item.name);
-                    console.log(`Price: $${item.price}`);
+    fetch(API_URL)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            // console.log(data.length);
+            data.forEach(item => {
+                // console.log(item.name);
+                // console.log(`Price: $${item.price}`);
 
-                    const itemContents = document.getElementById("item-contents");
+                const itemContents = document.getElementById("item-contents");
 
-                    let itemCard = document.createElement('div');
-                    itemCard.classList.add("item-card");
-                    itemContents.appendChild(itemCard);
+                let itemCard = document.createElement('div');
+                itemCard.classList.add("item-card");
+                itemContents.appendChild(itemCard);
 
-                    let itemImage = document.createElement('img');
-                    itemImage.classList.add("item-img");
-                    itemImage.src = item.image;
-                    itemImage.alt = "Item Image";
+                let itemImage = document.createElement('img');
+                itemImage.classList.add("item-img");
+                itemImage.src = item.image;
+                itemImage.alt = "Item Image";
 
-                    let itemName = document.createElement('h6');
-                    itemName.classList.add("item-name");
-                    itemName.textContent = item.name;
+                let itemName = document.createElement('h6');
+                itemName.classList.add("item-name");
+                itemName.textContent = item.name;
 
-                    let itemPrice = document.createElement('p');
-                    itemPrice.classList.add("item-price");
-                    itemPrice.textContent = `$${item.price}`;
+                let itemPrice = document.createElement('p');
+                itemPrice.classList.add("item-price");
+                itemPrice.textContent = `$${item.price}`;
 
-                    let addToCart = document.createElement('button');
-                    addToCart.classList.add("add-to-cart");
-                    addToCart.textContent = "Add";
+                let addToCartBtn = document.createElement('button');
+                // let addToCartBtn = document.createElement('div');
+                addToCartBtn.classList.add("add-btn");
+                addToCartBtn.textContent= "Add to cart";
+                // addToCartBtn.innerHTML = '<i class="fa-solid fa-cart-plus fa-xl"></i>&nbsp<i class="fa-solid fa-greater-than fa-xl"></i>';
 
-                    itemCard.appendChild(itemImage);
-                    itemCard.appendChild(itemName);
-                    itemCard.appendChild(itemPrice);
-                    itemCard.appendChild(addToCart);
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            })
-
-    }
-
-    let itemImage = document.getElementsByClassName("item-image");
-    let itemName = document.getElementsByClassName("item-name");
-    let itemPrice = document.getElementsByClassName("item-price");
+                itemCard.appendChild(itemImage);
+                itemCard.appendChild(itemName);
+                itemCard.appendChild(itemPrice);
+                itemCard.appendChild(addToCartBtn);
 
 
-    displayItems();
+                let quantity = 0;
+                function quantityCount() {
+                    quantity++;
+                }
+
+                // let itemsInCart = [];
+                // itemsInCart.push(
+                //         "image": item.image,
+                //         "name": item.name,
+                //         "price": item.price,
+                //         "quantity": quantity
+                // )
+
+                addToCartBtn.addEventListener("click", () => {
+                    // quantityCount();
+                    // console.log(quantity);
+                    // let itemsInCart = [
+                    //     {
+                    //         "image": item.image,
+                    //         "name": item.name,
+                    //         "price": item.price,
+                    //         "quantity": quantity
+                    //     }
+                    // ];
+                    // let itemsInCart = [];
+                    if (item.quantity === 0) {
+                        itemsInCart.push(item);
+                        quantityCount();
+                    } else {
+                        quantityCount();
+                    }
+                    // itemsInCart.push(item);
+                    console.log(itemsInCart);
+                    // console.log(quantity);
+                })
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        })
 
 
 })
