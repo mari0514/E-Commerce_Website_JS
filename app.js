8fd68f05eca7dfbd58fd77d263f6bf54379415ca
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-02-08 21:19:21 -0800

    Added some functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
-    // const API_URL = "http://jsonblob.com/api/1072684897273135104";
-    const API_URL = "http://jsonblob.com/api/1072766826269917184";
+    // const API_URL = "http://jsonblob.com/api/1072766826269917184";
+    const API_URL = "http://jsonblob.com/api/1073046958612168704";
 
     fetch(API_URL)
         .then(res => {
@@ -10,6 +10,7 @@ window.addEventListener("load", () => {
             // console.log(data.length);
             data.forEach(item => {
                 // console.log(item.name);
+                // console.log(item.id);
                 // console.log(`Price: $${item.price}`);
 
                 const itemContents = document.getElementById("item-contents");
@@ -43,41 +44,108 @@ window.addEventListener("load", () => {
                 itemCard.appendChild(addToCartBtn);
 
 
-                let quantity = 0;
-                function quantityCount() {
-                    quantity++;
-                }
 
-                // let itemsInCart = [];
-                // itemsInCart.push(
-                //         "image": item.image,
-                //         "name": item.name,
-                //         "price": item.price,
-                //         "quantity": quantity
-                // )
 
-                addToCartBtn.addEventListener("click", () => {
-                    // quantityCount();
-                    // console.log(quantity);
-                    // let itemsInCart = [
-                    //     {
-                    //         "image": item.image,
-                    //         "name": item.name,
-                    //         "price": item.price,
-                    //         "quantity": quantity
-                    //     }
-                    // ];
-                    // let itemsInCart = [];
-                    if (item.quantity === 0) {
-                        itemsInCart.push(item);
-                        quantityCount();
-                    } else {
-                        quantityCount();
+                addToCartBtn.addEventListener("click", (event) => {
+                    // kokokara
+                    let itemsInCart = [];
+
+                    const addedItemsList = document.getElementById("added-items");
+
+                    const cartSum = document.getElementById("price-sum");
+                    const items = document.querySelectorAll(".item-card");
+
+                    const countTheSum = function() {
+                        let sumPrice = 0;
+                        itemsInCart.forEach(item => {
+                            sumPrice += item.price;
+                        });
+                        return sumPrice;
                     }
-                    // itemsInCart.push(item);
+
+                    const updateShoppingCartHTML = function() {
+                        if (itemsInCart.length > 0) {
+                            let result = itemsInCart.map(item => {
+                                return `
+                                <li class="added-item">
+                                <img src="${item.image}">
+                                <div class="added-itetm-description">
+                                    <h4>${item.name}</h4>
+                                    <p>$${item.price} * ${item.count}</p>
+                                    <div>
+                                        <button class="minus-btn" data-id="${item.id}">-</button>
+                                        <button class="plus-btn" data-id="${item.id}">+</button>
+                                    </div>
+                                </div>
+                                </li>
+                                `;
+                            });
+                            addedItemsList.innerHTML = result.join('');
+                            // document.getElementById('cart').classList.remove('hidden');
+                            cartSum.innerHTML = "$" + countTheSum();
+                        } else {
+                            // document.getElementById('cart').classList.add('hidden');
+                            let emptyMessage = "<p>Your shopping cart is empty.</p>";
+                            addedItemsList.innerHTML = emptyMessage.join('');
+                            document.getElementById('item-counts').innerHTML = "0";
+                            cartSum.innerHTML = "$0";
+                        }
+                    }
+
+                    function updateItemsInCart(item) {
+                        for (let i = 0; i < itemsInCart.length; i++) {
+                            if (itemsInCart[i].id == item.id) {
+                                itemsInCart[i].count += 1;
+                                itemsInCart[i].price = itemsInCart[i].basePrice * itemsInCart[i].count;
+                                return;
+                            }
+                        }
+                        itemsInCart.push(item);
+                    }
+                    // kokomade
+
+
+                    if (event. target.classList.contains('add-btn')) {
+                        // const itemID = event.target.dataset.itemID;
+                        // // const itemName = item.querySelector(".item-name").innerHTML;
+                        // const itemName = document.getElementsByClassName("item-name");
+                        // // const itemPrice = item.querySelector(".item-price").innerHTML;
+                        // const itemPrice = document.querySelector(".item-price").innerHTML;
+                        // // const itemImage = item.querySelector(".item-img").src;
+                        // const itemImage = document.querySelector(".item-img").src;
+            
+                        let itemToCart = {
+                            name: item.name,
+                            image: item.image,
+                            id: item.id,
+                            count: 1,
+                            price: +item.price,
+                            basePrice: +item.price
+                        };
+            
+                        updateItemsInCart(itemToCart);
+                        updateShoppingCartHTML();
+                    }
+
+
+                    document.getElementById("item-counts").innerHTML = "1";
+
+
+                    console.log(item.name);
                     console.log(itemsInCart);
-                    // console.log(quantity);
-                })
+                    console.log(countTheSum());
+                });
+                
+
+                let shoppingCartButton = document.getElementById("shopping-cart");
+                shoppingCartButton.addEventListener("click", () => {
+                    document.getElementById("cart").style.display = "flex";
+                });
+
+                let closeButton = document.getElementById("close-btn");
+                closeButton.addEventListener("click", () => {
+                    document.getElementById("cart").style.display = "none";
+                });
             });
         })
         .catch(error => {
